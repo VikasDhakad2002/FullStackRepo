4e2c57a934f494ae2c4491182e95bcfc56ee3ac4
Vikas Dhakad <dhakadvikas626@gmail.com>
2024-02-28 17:02:01 +0530

    task1 from developer

--- GITProj01/src/main/java/com/nt/main/AppTest.java
+++ GITProj01/src/main/java/com/nt/main/AppTest.java
@@ -7,7 +7,7 @@ public class AppTest{
 	public static void main(String[] args) {
 		ArithmeticOperations ar = new ArithmeticOperations();
 		System.out.println("sum:"+ar.sum(100, 200));
-		System.out.println("sub:"+ar.sub(100, 200));
+		System.out.println("sub:"+ar.sub(100, 200)); 
 
 	}
 
