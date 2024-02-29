545200aeb493bb07b67c3cf22496e534c07bfdad
Vikas Dhakad <dhakadvikas626@gmail.com>
2024-02-29 18:43:38 +0530

    TL==B2==task

--- GITProj01/src/main/java/com/nt/reports/SalesReport.java
+++ GITProj01/src/main/java/com/nt/reports/SalesReport.java
@@ -5,4 +5,8 @@ public class SalesReport {
 	public void weeekReport() {
 		System.out.println("SalesReport.weeklyReport()");
 	}
+	
+	public void monthlyReport() {
+		System.out.println("SalesReport.monthlyReport()");
+	}
 }
