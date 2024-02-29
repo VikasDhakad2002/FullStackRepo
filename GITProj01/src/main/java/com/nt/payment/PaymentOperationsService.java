58ba9269c6cabcc26bc52b065999ab7c6dcd5f65
Vikas Dhakad <dhakadvikas626@gmail.com>
2024-02-29 16:26:01 +0530

    dev1-B1-task1

--- GITProj01/src/main/java/com/nt/payment/PaymentOperationsService.java
+++ GITProj01/src/main/java/com/nt/payment/PaymentOperationsService.java
@@ -2,4 +2,11 @@ package com.nt.payment;
 
 public class PaymentOperationsService {
 
+	public String doCreditCardPayment() {
+		return "Credit card Payment is done";
+	}
+	
+	public String doDebitCardPayment() {
+		return "Debit card payment is done";
+	}
 }
